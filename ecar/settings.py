4f7b14207f3bba45d691e1444da1006c882df0f7
Akshar <aksharp955@gmail.com>
2023-04-26 20:23:12 +0530

    cars

--- ecar/settings.py
+++ ecar/settings.py
@@ -143,5 +143,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587 
-EMAIL_HOST_USER = 'aksharp955@gmail.com'
-EMAIL_HOST_PASSWORD = 'gpejwskrjguvjklf'
